00e11363e905392283431813ffff41b7b1a3941b
SOOCHUL SON <zerozerobase@gmail.com>
2021-12-18 23:59:09 +0900

    design: 콘텐츠 내용 추가, 각 article 디자인

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-
 function textChange() {
+    /// Device에 따라 다른 문구 보여주기
+    const mql = window.matchMedia("screen and (max-width: 1300px)");
     document.querySelector(".cont-item").addEventListener("mouseover", () => {
-        document.querySelector("#header-msg").innerText = "All is well."
+        document.querySelector("#header-msg").innerText = mql.matches ? "Portfolio" : "Port- folio";
     })
     document.querySelector(".cont-item").addEventListener("mouseout", () => {
-        document.querySelector("#header-msg").innerText = "すべては よくなる。"
+        document.querySelector("#header-msg").innerText = "ポート フォリオ";
     })
-}
+};
+
+function menuMove() {
+    const indexLink = document.querySelector("#link-index");
+    const aboutLink = document.querySelector("#link-about");
+    const skillsLink = document.querySelector("#link-skills");
+    const worksLink = document.querySelector("#link-works");
+
+    indexLink.addEventListener("click", () => {
+        document.querySelector(".contents-index").classList.remove("off");
+        document.querySelector(".contents-about").classList.add("off");
+        document.querySelector(".contents-skills").classList.add("off");
+        document.querySelector(".contents-works").classList.add("off");
+    })
+    aboutLink.addEventListener("click", () => {
+        document.querySelector(".contents-index").classList.add("off");
+        document.querySelector(".contents-about").classList.remove("off");
+        document.querySelector(".contents-skills").classList.add("off");
+        document.querySelector(".contents-works").classList.add("off");
+    })
+    skillsLink.addEventListener("click", () => {
+        document.querySelector(".contents-index").classList.add("off");
+        document.querySelector(".contents-about").classList.add("off");
+        document.querySelector(".contents-skills").classList.remove("off");
+        document.querySelector(".contents-works").classList.add("off");
+    })
+    worksLink.addEventListener("click", () => {
+        document.querySelector(".contents-index").classList.add("off");
+        document.querySelector(".contents-about").classList.add("off");
+        document.querySelector(".contents-skills").classList.add("off");
+        document.querySelector(".contents-works").classList.remove("off");
+    })
+};
 
 textChange();
+menuMove();
